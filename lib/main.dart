db4a2d4bc04c5b14a0ec673b562a6c3267bcf06e
skoiyme <143878217+skoiyme@users.noreply.github.com>
2025-05-07 16:53:06 +0900

    커스텀위젯
    학습

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,54 +29,15 @@ class MyApp extends StatelessWidget {
               ],
             ),
 
-
-
-
-
         body: Container(
-          padding: EdgeInsets.all(34),
-          child: Row(
-            children: <Widget> [
-              //IMG
-              Column(
-                children: [
-                  Image(image: AssetImage('img.jpg',), width: 200, height: 200,)
-                ],
-              ),
-              Container(
-                padding: const EdgeInsets.all(13.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(width: 200, child:
-                    Text('에렌 DSLR 100D (진격의거인, 거인 16기가SD포함)',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold)),
-                    ),
-                    Text('성동구 행당동 끌올 10분전', style: TextStyle(color: Colors.grey),),
-                    Text('210,000원',style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(150, 0, 0, 0),
-                      child: Row(
-                        children: [
-                          IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border)),
-                          Text('4')
-                        ],
-                      ),
-                    )
-
-                  ],
-                ),
-              ),
-
-
-
-
-            ],
+          width: 120,
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Account();
+            },
           ),
-
         ),
-
-
-
 
 bottomNavigationBar: BottomNavigationBar(
   showUnselectedLabels: false,
@@ -98,9 +58,6 @@ bottomNavigationBar: BottomNavigationBar(
   ]
 ),
 
-
-
-
       ),
     );
 
@@ -109,3 +66,63 @@ bottomNavigationBar: BottomNavigationBar(
 }
 
 
+// 에렌예거
+class ShopItem extends StatelessWidget {
+  const ShopItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      padding: EdgeInsets.all(34),
+      child: Row(
+        children: <Widget> [
+          //IMG
+          Column(
+            children: [
+              Image(image: AssetImage('img.jpg',), width: 200, height: 200,)
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.all(13.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(width: 200, child:
+                Text('에렌 DSLR 100D (진격의거인, 거인 16기가SD포함)',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold)),
+                ),
+                Text('성동구 행당동 끌올 10분전', style: TextStyle(color: Colors.grey),),
+                Text('210,000원',style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Container(
+                  padding: EdgeInsets.fromLTRB(150, 0, 0, 0),
+                  child: Row(
+                    children: [
+                      IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border)),
+                      Text('4'),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//연락처목록
+class Account extends StatelessWidget {
+  const Account({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Icon(Icons.account_circle, size: 45,),
+        Text('장승권', style: TextStyle(fontSize: 15),)
+      ],
+    );
+  }
+}
